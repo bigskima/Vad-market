d82bb716ca9b5e37cda6e50f8b9a1e187bb78e1f
bigskima <our2026project@gmail.com>
2026-09-09 22:41:11 +0100

    feat(ui): clarify focused NGN deposit route

--- apps/mobile/src/app/wallet/deposit.tsx
+++ apps/mobile/src/app/wallet/deposit.tsx
@@ -3,7 +3,7 @@ import { ProductSubpage } from '@/features/navigation/product-subpage';
 
 export default function DepositRoute() {
   return (
-    <ProductSubpage title="Deposit">
+    <ProductSubpage title="Deposit NGN">
       <PaymentReadinessCard initialMode="DEPOSIT" lockMode />
     </ProductSubpage>
   );
